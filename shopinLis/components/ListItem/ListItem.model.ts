0c7d8e77a33f41ef6e38e4bb0a2f99a40ee3b90d
Radeonxz <xuanxxzhao@gmail.com>
2021-07-27 21:31:33 -0400

    chore: add editItem

--- shopinLis/components/ListItem/ListItem.model.ts
+++ shopinLis/components/ListItem/ListItem.model.ts
@@ -1,7 +1,16 @@
+export interface IItem {
+  id: number | null;
+  text: string | null;
+}
+
 export interface ListItemProps {
-  item: {
-    id: number;
-    text: string;
-  };
+  item: IItem;
+  isEditing: boolean;
+  editItemDetail: IItem;
+  checkedItems: Array<IItem> | [];
   deleteItem: (id: number) => void;
+  editItem: (id: number, text: string) => void;
+  saveEditItem: (id: number) => void;
+  handleEditChange: (text: string) => void;
+  itemChecked: (id: number, text: string) => void;
 }
